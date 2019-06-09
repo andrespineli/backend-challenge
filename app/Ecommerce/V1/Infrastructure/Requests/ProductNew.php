0fc6a62b0af9fb5055ec4ad8314c9db254621123
Andre Spineli <andrespineli@gmail.com>
2019-06-09 13:20:51 -0300

    Corrigindo valor minimo para o preço de um produto para que não seja menor que 0

--- app/Ecommerce/V1/Infrastructure/Requests/ProductNew.php
+++ app/Ecommerce/V1/Infrastructure/Requests/ProductNew.php
@@ -26,7 +26,7 @@ class ProductNew extends FormRequest
         return [
             'sku' => 'required|integer|unique:products,sku',
             'name' => 'required|string|unique:products,name|max:255',
-            'price' => 'required|numeric|min:1'
+            'price' => 'required|numeric|min:0'
         ];
     }
 }
